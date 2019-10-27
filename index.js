da89c98d1f5ca85ccbe9f515f9b5ae8fd0085880
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2019-10-27 19:39:49 +0530

    initial push

--- index.js
+++ index.js
@@ -4,11 +4,54 @@ class Node {
     this.value = value;
     this.right = null;
     this.left = null;
+    this.count = 1;
   }
 }
 class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    if (!value) return undefined;
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let node = this.root;
+      while (node) {
+        if (value < node.value) {
+          if (!node.left) {
+            node.left = newNode;
+            break;
+          }
+          node = node.left;
+        } else if (value > node.value) {
+          if (!node.right) {
+            node.right = newNode;
+            break;
+          }
+          node = node.right;
+        } else {
+          node.count += 1;
+          break;
+        }
+      }
+    }
+    return this;
+  }
+  find(value) {
+    if (!vlaue) return undefined;
+    if()
+  }
 }
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(10);
+tree.insert(7);
+tree.insert(15);
+tree.insert(3);
+tree.insert(14);
+tree.insert(17);
+tree.insert(8);
+console.log(tree);
 module.exports = BinarySearchTree;
